d2ea6aa4e3bd590f16066572de8ff630588d03f3
martins <gabrielamspinola@gmail.com>
2022-08-31 16:39:11 -0300

    chore: criação de rota e geração de token

--- src/routes/login.js
+++ src/routes/login.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const crypto = require('crypto');
 const emailValidation = require('../middleware/emailValidation');
 const passwordValidation = require('../middleware/passwordValidation');
 
 const router = express.Router();
 
 router.post('/', emailValidation, passwordValidation, (_req, res) => {
-  const generatedToken = Math.random().toString(36).substr(2, 16);
+  const generatedToken = crypto.randomBytes(8).toString('hex');
   
   return res.status(200).json({ token: generatedToken });
 });
