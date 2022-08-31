9957e9949d288a6836efee1283e2637ac71aa797
martins <gabrielamspinola@gmail.com>
2022-08-31 19:02:19 -0300

    fix: correção de cód, adição de validação de field

--- src/middleware/passwordValidation.js
+++ src/middleware/passwordValidation.js
@@ -1,10 +1,20 @@
 const passwordValidation = (req, res, next) => {
   const { password } = req.body;
+  const login = req.body; 
+
+  if (!login.password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < 1) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
   if (password.length >= 6) {
     return next();
   }
 
-  return res.status(400).json({ message: 'password must be at least 6 characters' });
+  return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
 };
 
 module.exports = passwordValidation;
