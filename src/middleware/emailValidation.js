1fb0a2c0d75c7427490b4a7a4d4880df68415585
martins <gabrielamspinola@gmail.com>
2022-08-31 19:02:37 -0300

    fix: adição de validação de field

--- src/middleware/emailValidation.js
+++ src/middleware/emailValidation.js
@@ -1,12 +1,22 @@
-const regexEmail = /^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$/;
+const regexEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
 
 const emailValidation = (req, res, next) => {
-    const { email } = req.body;
-    if (regexEmail.test(email)) {
-      return next();
-    }
+  const { email } = req.body;
+  const login = req.body;
 
-    return res.status(400).json({ message: 'invalid email' });
+  if (!login.email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (email.length < 1) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (regexEmail.test(email)) {
+    return next();
+  }
+
+  return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
 };
 
 module.exports = emailValidation;
